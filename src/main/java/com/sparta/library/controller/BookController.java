2d2a986601422b93c61fffe1c91e4e8bee4bea65
hajin428 <amm_321@naver.com>
2024-07-16 18:16:19 +0900

    member service

--- src/main/java/com/sparta/library/controller/BookController.java
+++ src/main/java/com/sparta/library/controller/BookController.java
@@ -3,6 +3,7 @@ package com.sparta.library.controller;
 import com.sparta.library.dto.AllBookResponseDto;
 import com.sparta.library.dto.BookRequestDto;
 import com.sparta.library.dto.BookResponseDto;
+import com.sparta.library.dto.SelectResponseDto;
 import com.sparta.library.service.BookService;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,9 +24,9 @@ public class BookController {
     // "/book" 경로의 post 요청 처리
     @PostMapping
     //요청된 json 타입의 body 데이터를 BookRequestDto 객체로 받아서 변환하여 받음
-    public BookResponseDto createBook(@RequestBody BookRequestDto requestDto) {
+    public BookResponseDto createBook(@RequestBody BookRequestDto bookrequestDto) {
         //bookService 클래스의 createBook 메서드를 실행한 후 BookResponse 객체로 반환
-        return bookService.createBook(requestDto);
+        return bookService.createBook(bookrequestDto);
     }
 
     //도서 목록 전체 조회
@@ -36,7 +37,7 @@ public class BookController {
 
     //게시글 선택 조회
     @GetMapping("/{id}")
-    public BookResponseDto getBookById(@PathVariable Long id) {
+    public SelectResponseDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
